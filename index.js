57c269a56ccafa2ff9de9686ddd0c4a2b11da49e
Chris Ashman <chrisashman96@gmail.com>
2020-07-14 17:25:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ function countDown( startingNumber ) {
     startingNumber -= 1;
   }
   console.log( startingNumber );
-=======
-// Code your solutions in this file
-const cards = (["Ada", "Brendan", "Ali"]. "birthday");
+======
 
-function writeCards(cards) {
-  for (let i = 0; i < cards.length; i++) {
-    console.log('Thank you, ${cards[i]} for the wonderful birthday gift');
-  }
 
 }
